c2ec8da231569e6dcd04b564a53a27e6283171ce
Eva <evsc1000@stud.ek.dk>
2026-02-27 08:52:09 +0100

    tilføjet js til index siden

---
+++ index.js
@@ -0,0 +1,16 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/categories";
+
+const container = document.querySelector("#link-container");
+
+function getData() {
+  fetch(endpoint)
+    .then((respons) => respons.json())
+    .then(showData);
+}
+function showData(data) {
+  console.log(data);
+  data.forEach((fisk) => {
+    container.innerHTML += `<a href="productlist.html?category=${fisk.category}">${fisk.category}</a>`;
+  });
+}
+getData();
